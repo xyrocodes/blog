5d80376fb6231bfa64817f3c2ac1db0d7a81c5af
anurag <anurag@xyrocodes.com>
2023-06-06 12:57:17 +0530

    Routes for the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import PostDetail from './PostDetail';
+import CreatePost from './CreatePost';
+
 function App() {
   return (
-    <div className="App">
-      Starting React Blog
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route exact path='/post/:postId' element={<PostDetail />}></Route>
+        <Route exact path='/create-post' element={<CreatePost />}></Route>
+      </Routes>
     </div>
   );
 }
